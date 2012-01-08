ddbafa1d6e344b3a06bcbb444170a40c4c801217
theRemix <jon.borgonia@gmail.com>
2012-01-07 16:18:56 -1000

    replaced the untyped() js with reflection

--- Haxenode.hx
+++ Haxenode.hx
@@ -2,6 +2,10 @@ import js.Node;
 class Haxenode
 {
 
+  private static inline var ENV_DEVELOPMENT = 'development'; // app.settings.env
+  private static inline var ENV_PRODUCTION = 'production'; 
+  private static inline var SERVER_LISTEN = 3117; // app.address().port
+
   public static function main()
   {
     var express = Node.require('express');
@@ -15,28 +19,25 @@ class Haxenode
       app.use(express.bodyParser());
       app.use(express.methodOverride());
       app.use(app.router);
-      app.use(untyped __js__("express.static(__dirname + '/public')"));
+      app.use(Reflect.callMethod(express,Reflect.field(express,"static"), [(Node.__dirname + '/public')])); // Reflection for reserved word 'static'
     });
-    
-    untyped __js__(
-      "app.configure('development', function(){
-          app.use(express.static(__dirname + '/public'));
-          app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-      });
 
-      app.configure('production', function(){
-        var oneYear = 31557600000;
-        app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-        app.use(express.errorHandler());
-      });"
-    );
+    Reflect.callMethod(app,app.configure,[ENV_DEVELOPMENT, function(){ // Reflection for ...rest params of app.configure
+        app.use(Reflect.callMethod(express,Reflect.field(express,"static"), [(Node.__dirname + '/public')]));
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    }]);
+    Reflect.callMethod(app,app.configure,[ENV_PRODUCTION, function(){
+        app.use(Reflect.callMethod(express,Reflect.field(express,"static"), [Node.__dirname + '/public', { maxAge: 31557600000 }]));
+        app.use(express.errorHandler(null));
+    }]);
 
     // Routes
 
     app.get('/', routes.index);
     
-    app.listen(3117);
+    app.listen(SERVER_LISTEN);
     
     Node.console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   }
+  
 }
