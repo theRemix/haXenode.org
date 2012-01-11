0829908ee7e38f11c44ffffe62a48e6052df4450
Jon Borgonia <jon@byhumanhand.com>
2012-01-10 17:43:32 -1000

    pops the "whatishaxe" help tips immediately, then animates out.

--- UserScript.hx
+++ UserScript.hx
@@ -3,17 +3,17 @@ import jQuery.JQuery;
 
 class UserScript
 {
-  private static inline var POP_SHOW_SPEED = 100;
+  private static inline var POP_OUT_SPEED = 100;
   private static inline var STARTING_POP_TOP = 760;
   private static inline var POP_ANIMATION_DIST = 14;
-
+  
   private var what_is_pop: JQuery; // collection
   private var what_is_list_item: JQuery; // collection 
 
   public inline function new()
   { 
     what_is_pop = new JQuery(".what_is_pop").css({opacity:0});
-    what_is_list_item = new JQuery("#whatishaxe-left ul li");
+    what_is_list_item = new JQuery("#whatishaxe-left ul li a");
     
     new JQuery(function():Void{
       what_is_list_item.mouseenter(show_what_is).mouseleave(close_what_is);
@@ -23,20 +23,28 @@ class UserScript
   private inline function show_what_is( event:Event ):Event
   {
     /*trace( event.target.getAttribute("id") );*/
-    what_is_pop.stop().animate({opacity:0, top:(STARTING_POP_TOP+POP_ANIMATION_DIST)+"px"},POP_SHOW_SPEED);
-    new JQuery("#"+event.target.getAttribute("id")+"_pop").stop().css(
-        {top:(STARTING_POP_TOP-POP_ANIMATION_DIST)+"px"}
-      ).animate(
-        {opacity:1, top:(STARTING_POP_TOP)+"px"},POP_SHOW_SPEED
-      );
+    
+    var selector = event.target.getAttribute("id")+"_pop";
+    new JQuery("#"+selector).css({opacity:1, top:STARTING_POP_TOP});
+    
+    // close any that may not have closed on mouseleave
+    var tmp:JQuery;
+    what_is_pop.each(function(idx, obj){
+      if(obj.id != selector){
+        tmp = new JQuery("#"+obj.id);
+        if(Std.parseInt(tmp.css("opacity")) > 0){
+          tmp.stop().animate({opacity:0, top:(STARTING_POP_TOP+POP_ANIMATION_DIST)+"px"}, POP_OUT_SPEED);
+        }
+      }
+    });
+    
     return event;
   }
   
   private inline function close_what_is( event:Event ):Event
   {
-    new JQuery("#"+event.target.getAttribute("id")+"_pop").stop().animate(
-      {opacity:0, top:(STARTING_POP_TOP+POP_ANIMATION_DIST)+"px"},POP_SHOW_SPEED
-    );
+    var selector = "#"+event.target.getAttribute("id")+"_pop";
+    new JQuery(selector).stop().animate({opacity:0, top:(STARTING_POP_TOP+POP_ANIMATION_DIST)+"px"}, POP_OUT_SPEED);
     return event;
   }
   
